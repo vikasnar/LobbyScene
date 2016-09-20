4def9c5c78ca8a2638fbbe5d362fe4d41a045fbf
VikasN <vikas.n649@gmail.com>
2016-09-19 17:32:20 -0700

    Door and key

--- _lobbyScene/Assets/Script/DoorScript.cs
+++ _lobbyScene/Assets/Script/DoorScript.cs
@@ -3,13 +3,77 @@ using System.Collections;
 
 public class DoorScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static bool doorKey;
+	public bool open;
+	public bool close;
+	public bool inTrigger;
+
+	void OnTriggerEnter(Collider other)
+	{
+		inTrigger = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnTriggerExit(Collider other)
+	{
+		inTrigger = false;
+	}
+
+	void Update()
+	{
+		if (inTrigger)
+		{
+			if (close)
+			{
+				if (doorKey)
+				{
+					if (Input.GetKeyDown(KeyCode.E))
+					{
+						open = true;
+						close = false;
+					}
+				}
+			}
+			else
+			{
+				if (Input.GetKeyDown(KeyCode.E))
+				{
+					close = true;
+					open = false;
+				}
+			}
+		}
+
+		if (open)
+		{
+			var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+			transform.rotation = newRot;
+		}
+		else
+		{
+			var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+			transform.rotation = newRot;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (inTrigger)
+		{
+			if (open)
+			{
+				GUI.Box(new Rect(0, 0, 200, 25), "Press E to close");
+			}
+			else
+			{
+				if (doorKey)
+				{
+					GUI.Box(new Rect(0, 0, 200, 25), "Press E to open");
+				}
+				else
+				{
+					GUI.Box(new Rect(0, 0, 200, 25), "Need a key!");
+				}
+			}
+		}
 	}
 }
